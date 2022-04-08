8f7db17ce433307872ae56206f37c3e8d28d58a1
surajb2008 <97325110+surajb2008@users.noreply.github.com>
2022-04-07 22:29:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ leftWristX = 0;
 
 function setup(){
     video = createCapture(VIDEO);
-    video.size(550, 550);
+    video.size(550, 500);
 
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
@@ -17,7 +17,7 @@ function setup(){
 }
 
 function modelLoaded(){
-    console.log('PoseNet Is Initialized');
+    console.log('PoseNet Is Initialized!');
 }
 
 function gotPoses(results)
